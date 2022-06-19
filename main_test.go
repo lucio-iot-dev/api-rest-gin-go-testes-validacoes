b6699705d56a6c5ffed538e1f863d227dbadf12a
Lúcio do Nascimento <luciomate95@gmail.com>
2022-06-19 16:11:44 -0300

    Testando a função EditaAluno

--- main_test.go
+++ main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
@@ -99,4 +100,23 @@ func TestDeletaAlunoHandler(t *testing.T) {
 	resposta := httptest.NewRecorder()
 	r.ServeHTTP(resposta, req)
 	assert.Equal(t, http.StatusOK, resposta.Code)
+}
+
+func TestEditaUmAlunoHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	CriaAlunoMock()
+	defer DeleteAlunoMock()
+	r := SetupDasRotasDeTeste()
+	r.PATCH("/alunos/:id", controllers.EditaAluno)
+	aluno := models.Aluno{Nome: "Nome do Aluno Teste", CPF: "47123456789", RG: "123456700"}
+	valorjson, _ := json.Marshal(aluno)
+	pathParaEditar := "/alunos/" + strconv.Itoa(ID)
+	req, _ := http.NewRequest("PATCH", pathParaEditar, bytes.NewBuffer(valorjson))
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	var alunoMockAtualiazdo models.Aluno
+	json.Unmarshal(resposta.Body.Bytes(), &alunoMockAtualiazdo)
+	assert.Equal(t, "47123456789", alunoMockAtualiazdo.CPF)
+	assert.Equal(t, "123456700", alunoMockAtualiazdo.RG)
+	assert.Equal(t, "Nome do Aluno Teste", alunoMockAtualiazdo.Nome)
 }

0177f43c26a1b8a34937e5c48e591a7707ce4103
Lúcio do Nascimento <luciomate95@gmail.com>
2022-06-19 12:24:53 -0300

    implementando o test da função ExibeTodosOsAlunos

--- main_test.go
+++ main_test.go
@@ -9,6 +9,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/lucio-iot-dev/api_rest_gin_go_testes_validacoes/controllers"
+	"github.com/lucio-iot-dev/api_rest_gin_go_testes_validacoes/database"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -28,3 +29,13 @@ func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
 	respostaBody, _ := ioutil.ReadAll(resposta.Body)
 	assert.Equal(t, mockDaResposta, string(respostaBody))
 }
+
+func TestListandoTodosOsAlunosHandler(t *testing.T) {
+	database.ConectaComBancoDeDados()
+	r := SetupDasRotasDeTeste()
+	r.GET("/alunos", controllers.ExibeTodosAlunos)
+	req, _ := http.NewRequest("GET", "/alunos", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code)
+	}
